3aac5b13ca779333135b3cf5d8a8c6f1beb80dd5
Efredis Lopez <efredis@gmail.com>
2021-03-11 22:01:23 -0300

    Proyecto de pruebas automatizadas - Web Service - Cypress - js (Mejorado)

--- cypress/integration/BaufestTestWs/SwaggerTest.js
+++ cypress/integration/BaufestTestWs/SwaggerTest.js
@@ -1,65 +1,92 @@
-describe('REST Test with Cypress', () => {
+describe('REST API Test with Cypress', () => {
 
-    //Agregar una mascota realizando un POST al path /v2/pet.
-    it('Hit al post', () => {
-      cy.request('POST','https://petstore.swagger.io/v2/pet',{
-        "id": 0,
-        "category": {
-          "id": 0,
-          "name": "Perros"
-        },
-        "name": "Baufi",
-        "photoUrls": [
-          "string"
-        ],
-        "tags": [
-          {
-            "id": 0,
-            "name": "Collie"
-          }
-        ],
-        "status": "available"
-      }
-      
-    ).as('pet')
-      cy.get('@pet').its('status').should('equal', 200) //Validando status Code
-      cy.get('@pet').its('body').should('include', {name: "Baufi"}) 
-  })
 
-//Realizar un GET /v2/pet/{petId} para obtener una mascota existente.
-  it('Hit al get', () => {
-    cy.request({
-        method: 'GET',
-        url: 'https://petstore.swagger.io/v2/pet/9222968140497186000',
-        //failOnStatusCode: false
-    }).as('pet')
-    cy.get('@pet').its('status').should('equal', 200) //Validando status Code
-    
-})
+  var myID = '';
 
-//Modificar una mascota existente mediante PUT al path /v2/pet.
-it('Hit al put', () => {
-    cy.request('PUT',
-          'https://petstore.swagger.io/v2/pet',   
-    {
-      "id": 9222968140497186000,
+//Agregar una mascota realizando un POST al path /v2/pet.
+  it('Hit al post', () => {
+    //Almacenando Variables
+    var method = 'POST';
+    var url = 'https://petstore.swagger.io/v2/pet';
+    var body = {
+      "id": 0,
       "category": {
         "id": 0,
         "name": "Perros"
       },
-      "name": "Nombre modificado",
+      "name": "Felipe",
       "photoUrls": [
         "string"
       ],
       "tags": [
         {
           "id": 0,
-          "name": "Crica"
+          "name": "perro"
         }
       ],
       "status": "available"
-    }).as('pet')
-    cy.get('@pet').its('status').should('equal', 200) //Validando status Code
-    cy.get('@pet').its('body').should('include', {name: "Nombre modificado"})    
+    }; 
+    
+
+    cy.request(method, url, body).as('pet');
+    
+    
+    cy.get('@pet')
+      .then(response => {
+        console.log('id: ', response.body.id)
+        myID = response.body.id; //Guardando Id
+
+      })
+    cy.get('@pet').its('status').should('equal', 200)
+    
+    cy.get('@pet').its('body').should('include', {name: "Felipe"})
+    })
+
+//Realizar un GET /v2/pet/{petId} para obtener una mascota existente.
+it('Hit al get', () => {
+
+
+  console.log('myID para get: ', myID); 
+  cy.request({method: 'GET',
+        url: `https://petstore.swagger.io/v2/pet/${myID}`, 
+        failOnStatusCode: false
+  
+  }).as('pety')
+  cy.get('@pety').its('status').should('equal', 200) //Validando status Code
+  
 })
+
+
+//Modificar una mascota existente mediante PUT al path /v2/pet.
+it('Hit al put', () => {
+
+  var method = 'PUT';
+  var url = `https://petstore.swagger.io/v2/pet`; 
+  var bodyPut = {
+    "id": myID,
+    "category": {
+      "id": 0,
+      "name": "Perros"
+    },
+    "name": "Nombre modificado con variables",
+    "photoUrls": [
+      "string"
+    ],
+    "tags": [
+      {
+        "id": 0,
+        "name": "perro"
+      }
+    ],
+    "status": "available"
+  }
+  
+
+  cy.request(method, url, bodyPut).as('pet')
+  cy.get('@pet').its('status').should('equal', 200) //Validando status Code
+  cy.get('@pet').its('body').should('include', {name: "Nombre modificado con variables"})
+  
+
+})
+
 })
